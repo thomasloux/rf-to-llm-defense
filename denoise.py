6c82c42a6d45b6fd55dd1eb57136cdee15508fed
gregoirebechade <gregoire.bechade@gmail.com>
2024-11-30 17:55:55 +0100

    add a noise function

--- denoise.py
+++ denoise.py
@@ -1,6 +1,7 @@
 import noisereduce as nr
 import numpy as np
 import scipy
+from scipy.io import wavfile
 
 
 
@@ -13,4 +14,20 @@ def denoise(signal : np.array, frqcy : int ) -> np.array:
     voice_only_speak=np.array([signal[i] for i in range(len(signal)) if sum[int(i/nperseg)]>0.1 ])
     noise_only=np.array([signal[i] for i in range(len(signal)) if sum[int(i/nperseg)]<0.1 ])
     return nr.reduce_noise(y=voice_only_speak, sr=frqcy, y_noise=noise_only)
+
+
+def noise_signal(signal, noise, add_gaussian=False): 
+    noise=np.concatenate([noise]*int(len(signal)/len(noise)+1))
+    gaussian = np.random.normal(0, 1, len(signal))
+    gaussian = gaussian/np.max(gaussian)
+    return signal + 2*noise[0:len(signal)] + (gaussian if add_gaussian else 0)*0.2
+
+
+
+noise_file='radio_noise.wav'
+noise=scipy.io.wavfile.read('data/'+noise_file)
+frqcy_noise=noise[0]
+noise=noise[1].T[0]
+noise=noise/np.max(np.abs(noise))
+
     
