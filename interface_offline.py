471e10f5d546a8c6bfbb475356158f5745d792a0
thomas.loux <thomas.loux@polytechnique.edu>
2024-12-01 09:05:05 +0100

    WIP

--- interface_offline.py
+++ interface_offline.py
@@ -3,6 +3,9 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 import numpy as np
 import torch
 from scipy.io.wavfile import write
+from dotenv import load_dotenv
+import os
+from mistralai import Mistral
 
 from denoise import denoise, get_speech, radio_noise
 
@@ -48,7 +51,7 @@ def transcribe(new_chunk):
     sr, y = new_chunk
 
     # Add noise
-    # y = radio_noise(y, add_gaussian=True)
+    y = radio_noise(y, add_gaussian=True)
     
     # Convert to mono if stereo
     if y.ndim > 1:
@@ -69,18 +72,87 @@ def transcribe(new_chunk):
     write("stream.wav", sr, stream)
     write("speech.wav", sr, speech)
     write("denoised.wav", sr, denoised)
-    return transcriber({"sampling_rate": sr, "raw": denoised})["text"]
+    return transcriber({"sampling_rate": sr, "raw": stream})["text"]
+
+
+def total_pipeline(messages, audio):
+    """
+    args:
+    list: privous sumaries
+    audio: new audio chunk
+    """
+    transcription = transcribe(audio)
+    if len(messages) == 0:
+        prompt = f"""
+You are a radio operator in the military. Your role is to transcribe and summarize messages from text. First provide a global summary of messages received (one bullet point per message).
+ For the last communication only, you need to provide the following information if provided only, using previous messages as context.
+- Date: (Date of the message.)
+- Time: (Timestamp of the message.)
+- Category of message: (The type of message being sent, for instance "Securite", "Danger", "Mayday", "Ennemi Presence", "Injured Soldier", "Contact with Ennemi".)
+- Reason for call: (The purpose of the transmission.)
+- Sender: (Information about the sender.)
+- Destination: (The recipient of the message.)
+- Position: (Coordinates or location details.)
+- Task: (What will be done by the sender.)
+- Ennemi presence: (Details about the enemy presence and its vehicules.)
+- Urgency level: (The urgency of the message, if mentioned.)
+"""
+        messages = [{"role": "system", "content": prompt}]
+    client = Mistral(api_key=os.getenv("API_KEY"))
+    model = "mistral-large-latest"
+
+    messages.append({"role": "user", "content": f"<<{transcription}>>"})
+    response = client.chat.complete(
+        model=model,  # ou un autre modèle disponible
+        messages=messages,
+    )
+    resp = (response.choices[0].message.content)
+    # messages.append({"role": "assistant", "content": resp})
+
+    return messages, resp, transcription, "\n".join([m["content"] for m in messages])
+
+
+def toggle_visibility(all_summaries, isVisible):
+    """Toggle the visibility of the 'all_summaries' textbox."""
+    return gr.update(visible=not isVisible), not isVisible
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     transcriber = get_model()
+    gr.set_static_paths(paths=["assets/"])
+    image_path = "assets/logo.jpeg"
+    # recording = gr.Audio(sources=["microphone"])
 
-    recording = gr.Audio(sources=["microphone"])
+    # demo = gr.Interface(
+    #     transcribe,
+    #     [recording],
+    #     ["text"],
+    #     title=logo_html
+    # )
+    with gr.Blocks() as demo:
+        with gr.Row():  # Add a row for the logo
+            gr.Image(image_path, label="Logo")
+        with gr.Row():  # Add the audio input and text output
+            recording = gr.Audio(sources=["microphone"], label="Record Your Audio")
+        with gr.Row():
+            transcription = gr.Textbox(label="Transcription")
+        with gr.Row():
+            messages = gr.State(value=[])
+            output = gr.Markdown(label="Rapport actuel")
+        with gr.Row():
+            all_summaries = gr.Textbox(label="Rapport complet", visible=False)
+        with gr.Row():
+            show_button = gr.Button("Show Full Report")
+            is_visible = gr.State(value=False)
+            show_button.click(toggle_visibility, inputs=[all_summaries, is_visible], outputs=[all_summaries, is_visible])
 
-    demo = gr.Interface(
-        transcribe,
-        [recording],
-        ["text"]
-    )
 
+        # messages = []
+        # Button for triggering the transcription
+        btn = gr.Button("Transcribe")
+        btn.click(total_pipeline, inputs=[messages, recording], outputs=[messages, output, transcription, all_summaries])
+
+    # Launch the app
     demo.launch()
